4c270d35d379f3b147f43b7ece14995f26dac60b
jljorgenson18 <jljorgenson18@gmail.com>
2017-05-09 02:10:53 -0500

    Adding proper params and implementing a proper dead letter promise chain

--- index.js
+++ index.js
@@ -1,11 +1,60 @@
+const Promise = require('bluebird');
+const get = require('lodash/get');
+const AWS = require('aws-sdk');
 
-exports.handleEventRecords = (records = [], handleEvent, whiteList) => {
-  const kinesisRecords = records.filter(rec => rec.kinesis && rec.kinesis.data);
-  const handlePromises = kinesisRecords.map(rec => {
-    const eventData = extractEventData(rec.kinesis.data);
-    if(eventData && eventData.type && (!whiteList || whiteList[eventData.type])) {
-      return handleEvent(eventData, rec.kinesis.approximateArrivalTimestamp);
+const extractEventData = (data) => {
+  let payload = null;
+  try {
+    payload = Buffer.from(data, 'base64').toString();
+    payload = JSON.parse(payload);
+  } catch (err) {}
+  return payload;
+};
+
+exports.handleEventRecords = (params) => {
+  const {records = [], handleEvent, whiteList, deadLetter, typeKey = 'type'} = params;
+  let deadLetterKinesis;
+  if(!handleEvent) {
+    throw new Error('handleEvent is required');
+  }
+  if(deadLetter && deadLetter.config) {
+    deadLetterKinesis = new AWS.Kinesis(deadLetter.config);
+  }
+  const kinesisRecords = records.filter(rec => get(rec, 'kinesis.data', false));
+  return Promise.map(kinesisRecords, record => {
+    const eventData = extractEventData(record.kinesis.data);
+    const eventType = get(eventData, typeKey, null);
+    if(whiteList && (!eventType || !whiteList[eventType])) {
+      return;
     }
-    return Promise.resolve();
+    return handleEvent(eventData, record.kinesis.approximateArrivalTimestamp).catch(err => {
+      return Promise.resolve({
+        failed: true,
+        eventData,
+        eventTimeStamp: record.kinesis.approximateArrivalTimestamp,
+        partitionKey: record.kinesis.partitionKey,
+        err
+      });
+    });
+  }).then(results => {
+    const failedResults = results.filter(result => result.failed);
+    if(deadLetterKinesis && deadLetter.StreamName) {
+      const deadLetterRecords = failedResults.map(result => {
+        const {eventData, eventTimeStamp, partitionKey, err} = result;
+        return {
+          Data: JSON.stringify({
+            eventData,
+            eventTimeStamp,
+            errMessage: err.message,
+            errStack: err.stack
+          }),
+          PartitionKey: partitionKey
+        };
+      });
+      return deadLetterKinesis.putRecords({
+        StreamName: deadLetter.StreamName,
+        Records: deadLetterRecords
+      }).promise();
+    }
   });
 };
